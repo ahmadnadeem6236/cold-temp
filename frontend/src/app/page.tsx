8ac828c5212dd281c01f2cb5330fb96f3c983e2e
ahmadnadeem6236 <ahmadnadeem6236@gmail.com>
2024-12-07 14:44:21 +0530

    setup init

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="bg-red-500">
+      <p>helo</p>
+      <p>Nadeem Ahmad</p>
+      <p>HEllo</p>
+      <Button variant={"outline"}>Hello</Button>
+    </div>
+  );
+}
